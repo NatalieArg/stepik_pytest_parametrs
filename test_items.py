fec154e69f371721a80ace9b0045330618c90cd4
NatalieArg <57195306+NatalieArg@users.noreply.github.com>
2019-11-20 21:12:12 +0300

    добавлен файл конфигурации и файл с тестом на проверку кнопки для разных языков

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue2/coders-at-work_207/"
+
+def test_check_add_item_button(browser):
+    browser.get(link)
+    button=len(browser.find_elements_by_css_selector('button[type="submit"]'))
+    assert button>1, 'Кнопка добавления товара не найдена'
+
+
